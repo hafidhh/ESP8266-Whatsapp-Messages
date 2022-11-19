91f55701edbcd50606a2d806dfb4bfe46cc2024b
hafidh7 <hafidh_170798@yahoo.co.id>
2022-11-19 18:29:54 +0700

    add Facebook Messages

--- include/CallMeBot.h
+++ include/CallMeBot.h
@@ -4,9 +4,33 @@
 #include <UrlEncode.h>
 
 void WhatsApp(String phoneNumber, String apiKey, String message) {
-
   // Data to send with HTTP POST
   String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+  WiFiClient client;    
+  HTTPClient http;
+  http.begin(client, url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
+
+void Facebook(String apiKey, String message) {
+  // Data to send with HTTP POST
+  String url = "https://api.callmebot.com/facebook/send.php?apikey=" + apiKey + "&text=" + urlEncode(message);
   WiFiClient client;    
   HTTPClient http;
   http.begin(client, url);
