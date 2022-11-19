13a1fc4b0d3dbea89a0daf0adedd57936de13b57
hafidh7 <hafidh_170798@yahoo.co.id>
2022-11-19 19:41:34 +0700

    Telegram Call example

---
+++ example/TelegramCall.cpp
@@ -0,0 +1,31 @@
+// Example TelegramCall
+// Github : 
+// https://github.com/hafidhhidayat
+// https://github.com/hafidhhidayat/ESP8266-Whatsapp-Messages
+
+#include <ESP8266WiFi.h>
+#include <CallMeBot.h>
+
+const char* ssid = "your_ssid";
+const char* password = "your_password";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Telegram Call
+  TelegramCall("@username", "message", "en-GB-Standard-B", "2", "yes", "30");
+}
+
+void loop() {
+  
+}
